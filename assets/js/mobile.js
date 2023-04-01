2f2a865892d8f6def0f2e932af7001ca227e52aa
enso-x <ggsi.anonymous@gmail.com>
2023-04-01 10:27:14 +0300

    Refactor

--- assets/js/mobile.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-	const mobileMenuButton = document.querySelector('.header__mobile-menu-button');
-	const mobileMenuContainer = document.querySelector('.header__content');
-
-	mobileMenuButton.addEventListener('click', () => {
-		mobileMenuContainer.classList.toggle('header__content:active');
-	});
-});
--- assets/js/phoneMask.js
+++
@@ -1,5 +0,0 @@
-const phoneInput = document.querySelector('input[name="mobile"]');
-Inputmask({
-	mask: '+7 (999) 999-99-99',
-	showMaskOnHover: false,
-}).mask(phoneInput);
